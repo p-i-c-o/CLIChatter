6adf8cc076cbfbbfd20c535f826ea3c500140e0d
p-i-c-o <95228665+p-i-c-o@users.noreply.github.com>
2022-07-25 00:46:38 +0200

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 os.system(f'echo "{dt_string}" >> log.txt')
 
 # server's IP address
-SERVER_HOST = "192.168.1.108"
+SERVER_HOST = "" #IP ADDRESS
 SERVER_PORT = 5002 # port we want to use
 separator_token = "<SEP>" # we will use this to separate the client name & message
 
